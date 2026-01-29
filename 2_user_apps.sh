7853f73e905fcb3f3d48a1dceda659974c9f5a9c
Gastón Martínez <gastongmartinez@gmail.com>
2026-01-29 10:14:30 -0300

    Instala rust-analyzer via rustup

--- 2_user_apps.sh
+++ 2_user_apps.sh
@@ -22,8 +22,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 if [ ! -d ~/.local/bin ]; then
     mkdir -p ~/.local/bin
 fi
-curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
-chmod +x ~/.local/bin/rust-analyzer
+rustup component add rust-analyzer
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # Flatpak
